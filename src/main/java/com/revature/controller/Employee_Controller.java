c4907ca1c288019a8b714cc20cfe7c5964ac5b57
idanzigm <idanzigm@gmail.com>
2022-01-25 23:22:14 -0500

    update to service and control layer

--- src/main/java/com/revature/controller/Employee_Controller.java
+++ src/main/java/com/revature/controller/Employee_Controller.java
@@ -1,8 +1,17 @@
 package com.revature.controller;
 
+import com.revature.service.Employee_Service;
 import io.javalin.Javalin;
+import io.javalin.http.Handler;
 
 public class Employee_Controller extends Controller{
+
+    Employee_Service employeeService = new Employee_Service();
+
+    Handler viewPastTickets = (ctx) -> {};
+
+    Handler ReimbursementRequest = (ctx) -> {};
+
     @Override
     public void addRoutes(Javalin app) {
 
