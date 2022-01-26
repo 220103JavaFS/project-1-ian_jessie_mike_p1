b0fa3afc1e7d0db50fdc22f3ccd34049be7472d9
idanzigm <idanzigm@gmail.com>
2022-01-25 19:28:58 -0500

    test commit

---
+++ src/main/java/com/revature/controller/Employee_Controller.java
@@ -0,0 +1,10 @@
+package com.revature.controller;
+
+import io.javalin.Javalin;
+
+public class Employee_Controller extends Controller{
+    @Override
+    public void addRoutes(Javalin app) {
+
+    }
+}
---
+++ src/main/java/com/revature/controller/Financial_Manager_Controller.java
@@ -0,0 +1,12 @@
+package com.revature.controller;
+
+import io.javalin.Javalin;
+
+public class Financial_Manager_Controller extends Controller{
+
+
+    @Override
+    public void addRoutes(Javalin app) {
+
+    }
+}
