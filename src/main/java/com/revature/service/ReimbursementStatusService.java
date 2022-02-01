c331a08e2a5e608940cb280a419ed58d9de45aaf
idanzigm <idanzigm@gmail.com>
2022-01-31 22:23:26 -0500

    service tests done

--- src/main/java/com/revature/service/ReimbursementStatusService.java
+++ src/main/java/com/revature/service/ReimbursementStatusService.java
@@ -4,7 +4,11 @@ import com.revature.dao.IReimbursement_Status_DAO;
 import com.revature.dao.Reimbursement_Status_DAO_IMP;
 
 public class ReimbursementStatusService {
-    private final IReimbursement_Status_DAO reimbursement_status_dao;
+    private static IReimbursement_Status_DAO reimbursement_status_dao;
+
+    ReimbursementStatusService(IReimbursement_Status_DAO reimbursement_status_dao){
+        ReimbursementStatusService.reimbursement_status_dao = reimbursement_status_dao;
+    }
 
     public ReimbursementStatusService(){reimbursement_status_dao = new Reimbursement_Status_DAO_IMP();}
 
